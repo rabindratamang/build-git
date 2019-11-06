1519211bc6ed8b3513578828dad61e030eef245b
rabindratamang <rabindra.tamang@yarshastudio.com>
2019-11-07 00:11:47 +0545

    finish part one

--- js/main.js
+++ js/main.js
@@ -8,7 +8,10 @@
     function Git(name){
         this.name = name;
         this.lastCommitId = -1;
-        this.HEAD = null; //pointer to the last commit 
+        this.branches = [];
+        var master = new Branch("master", null);
+        this.branches.push(master);
+        this.HEAD = master; //pointer to the last commit now points to current branch 
     }
 
     /**
@@ -43,8 +46,10 @@
      * @returns
      */
     Git.prototype.commit = function(message){
-        var commit = new Commit(++this.lastCommitId, this.HEAD, message);
-        this.HEAD = commit;
+        var commit = new Commit(++this.lastCommitId, this.HEAD.commit, message);
+        // this.HEAD = commit; //update head no longer reference commit but the current branches
+        this.HEAD.commit = commit;
+
         return commit;
     }
 
@@ -53,13 +58,35 @@
      */
     Git.prototype.log = function(){
             var history = [];
-            var commit = this.HEAD;
+            var commit = this.HEAD.commit;
 
             while(commit){
                 history.push(commit);
                 commit = commit.parent;
             }
             return history;
+    }
+
+    /**
+     *
+     *
+     * @param {string} branchName
+     */
+    Git.prototype.checkout = function(branchName){
+        for (var i = this.branches.length; i--; ) {
+            if (this.branches[i].name === branchName) {
+              console.log("Switched to existing branch: " + branchName);
+              this.HEAD = this.branches[i];
+              return this;
+            }
+          }
+
+        //if no branches found create newone
+        var newBranch = new Branch(branchName, this.HEAD.commit);
+        this.branches.push(newBranch);
+        this.HEAD = newBranch;
+        console.log("Switched to new branch: " + branchName);
+        return this;
     }
 
     //expose Git to window to access globally
