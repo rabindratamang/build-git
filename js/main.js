51e4b745d5e4e32e786a6c7d372bd3207887509e
rabindratamang <rabindra.tamang@yarshastudio.com>
2019-11-06 22:51:58 +0545

    add docs annotation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
+/**
+ *
+ *
+ * @param {string} name
+ */
 function Git(name){
     this.name = name;
     this.lastCommitId = -1;
 }
-
+/**
+ *
+ *
+ * @param {number} id
+ * @param {string} message
+ */
 function Commit(id,message) {
     this.id = id;
     this.message = message;
     // Assume that 'this' has a 'change' property too.
 }
 
+/**
+ *
+ *
+ * @param {string} message
+ * @returns
+ */
 Git.prototype.commit = function(message){
     var commit = new Commit(++this.lastCommitId,message);
     return commit;
